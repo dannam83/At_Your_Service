512bbee4c4b42096ee3f08c891d852d4687d6a14
Daniel Nam <dannam83@gmail.com>
2017-11-07 21:52:15 -0500

    Prepare for portfolio

--- lib/session.rb
+++ lib/session.rb
@@ -1,8 +1,7 @@
 require 'json'
 
 class Session
-  # find the cookie for this app
-  # deserialize the cookie into a hash
+
   def initialize(req)
     session_cookie = req.cookies["_rails_lite_app"]
 
@@ -21,8 +20,6 @@ class Session
     @data[key] = val
   end
 
-  # serialize the hash into json and save in a cookie
-  # add to the responses cookies
   def store_session(res)
     session_cookie = { path: '/', value: @data.to_json }
     res.set_cookie("_rails_lite_app", session_cookie)
