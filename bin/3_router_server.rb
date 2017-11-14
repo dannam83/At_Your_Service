713fcdf7cade05359f0df6d0e6ef3c021ff2db2a
Daniel Nam <dannam83@gmail.com>
2017-11-14 17:04:20 -0500

    add another cat

--- bin/3_router_server.rb
+++ bin/3_router_server.rb
@@ -5,13 +5,17 @@ require_relative '../lib/router'
 
 $cats = [
   { id: 1, name: "Curie" },
-  { id: 2, name: "Markov" }
+  { id: 2, name: "Markov" },
+  { id: 3, name: "Boris" }
 ]
 
 $statuses = [
   { id: 1, cat_id: 1, text: "Curie loves string!" },
   { id: 2, cat_id: 2, text: "Markov is mighty!" },
-  { id: 3, cat_id: 1, text: "Curie is cool!" }
+  { id: 3, cat_id: 1, text: "Curie is cool!" },
+  { id: 4, cat_id: 1, text: "Curie will curl into any box!" },
+  { id: 5, cat_id: 3, text: "Boris thinks he's king!" },
+  { id: 6, cat_id: 3, text: "Boris is bossy." }
 ]
 
 class StatusesController < ControllerBase
